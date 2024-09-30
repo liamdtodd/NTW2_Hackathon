e6256291471f222acb232eff895b28ff777e5579
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2024-09-30 10:16:55 -0700

    react router and routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+import Root from './Root.jsx';
+import ErrorPage from './ErrorPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <Root><ErrorPage /></Root>,
+    children: [
+      { index: true, element: <App /> },
+    ]
+  }
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
