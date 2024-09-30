4ec515e95c3e70c5d203e5267bf60829c709e23f
liamdtodd <91576595+liamdtodd@users.noreply.github.com>
2024-09-30 11:00:21 -0700

    add styles

--- src/Root.jsx
+++ src/Root.jsx
@@ -1,24 +1,21 @@
 import { NavLink, Outlet } from "react-router-dom";
+import './styles/Root.css';
 
 export default function Root(props) {
     const { children } = props;
 
     return (
         <>
-            <div>
-                <h1><NavLink to='/'>Capital City Athletic Club</NavLink></h1>
-                <nav>
-                    <h3><NavLink to='/sports'>Sports</NavLink></h3>
-                    <h3><NavLink to='contact'>Contact</NavLink></h3>
-                    <h3><NavLink to='/about'>About</NavLink></h3>
+            <div className="header-container">
+                <h1 className="site-title"><NavLink className='nav-link' to='/'>Capitol City Athletic Club</NavLink></h1>
+                <nav className="navbar">
+                    <h3 className="nav-item"><NavLink to='/sports' className='nav-link-item'>Sports</NavLink></h3>
+                    <h3 className="nav-item"><NavLink to='contact' className='nav-link-item'>Contact</NavLink></h3>
+                    <h3 className="nav-item"><NavLink to='/about' className='nav-link-item'>About</NavLink></h3>
                 </nav>
             </div>
 
             <main>{children || <Outlet />}</main>
-
-            <footer>
-
-            </footer>
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import './styles/index.css';
 
 import App from './App.jsx';
 import Root from './Root.jsx';
